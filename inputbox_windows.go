20cb42f75cbbd43961286b0d1e99e1b3ee4febaa
Test <test@Tests-MacBook-Pro.local>
2025-06-08 15:48:16 +0530

    popup box change for windows

--- inputbox_windows.go
+++ inputbox_windows.go
@@ -9,23 +9,39 @@ import (
 
 // InputBox displays a dialog box, returning the entered value and a bool for success
 func InputBox(title, message, defaultAnswer string) (string, bool) {
-	shell, err := ps.New(&backend.Local{})
-	if err != nil {
-		panic(err)
-	}
-	defer shell.Exit()
+	for {
+		shell, err := ps.New(&backend.Local{})
+		if err != nil {
+			panic(err)
+		}
+		defer shell.Exit()
 
-	out, _, err := shell.Execute(`
-		[void][Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
-		$title = '` + title + `'
-		$msg = '` + message + `'
-		$default = '` + defaultAnswer + `'
-		$answer = [Microsoft.VisualBasic.Interaction]::InputBox($msg, $title, $default)
-		Write-Output $answer
+		out, _, err := shell.Execute(`
+			[void][Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
+			$title = '` + title + `'
+			$msg = '` + message + `'
+			$default = '` + defaultAnswer + `'
+			$SENTINEL = '[CANCELLED]'
+			$answer = [Microsoft.VisualBasic.Interaction]::InputBox($msg, $title, $default)
+			if ($answer -eq '') {
+				Write-Output $SENTINEL
+			} else {
+				Write-Output $answer
+			}
 		`)
-	// FIXME: if cancel button is pressed in dialog, we should return false
-	if err != nil {
-		return "", false
+
+		if err != nil {
+			return "", false
+		}
+
+		result := strings.TrimSpace(string(out))
+		if result == "[CANCELLED]" {
+			return "", false
+		}
+		if result != "" {
+			return result, true
+		}
+
+		message = "Input cannot be empty. Please enter a value:"
 	}
-	return strings.TrimSpace(string(out)), true
 }
