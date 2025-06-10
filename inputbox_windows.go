d3c13e8e0fa05eb69c6c0745205dc9e780d1eba6
Test <test@Tests-MacBook-Pro.local>
2025-06-11 01:12:04 +0530

    fix inputbox library

--- inputbox_windows.go
+++ inputbox_windows.go
@@ -10,7 +10,7 @@ import (
 // InputBox displays a dialog box, returning the entered value and a bool for success
 func InputBox(title, message, defaultAnswer string) (string, bool) {
 	for {
-		message = message + "If you hit cancel button it will snooz for 4hrs."
+		message = message + " If you hit cancel button it will snooz for 4hrs."
 		shell, err := ps.New(&backend.Local{})
 		if err != nil {
 			panic(err)
@@ -22,27 +22,22 @@ func InputBox(title, message, defaultAnswer string) (string, bool) {
 			$title = '` + title + `'
 			$msg = '` + message + `'
 			$default = '` + defaultAnswer + `'
-			$SENTINEL = '[CANCELLED]'
 			$answer = [Microsoft.VisualBasic.Interaction]::InputBox($msg, $title, $default)
-			if ($answer -eq '') {
-				Write-Output $SENTINEL
-			} else {
-				Write-Output $answer
-			}
-		`)
-
+			Write-Output $answer
+			`)
 		if err != nil {
 			return "", false
 		}
-
-		result := strings.TrimSpace(string(out))
-		if result == "[CANCELLED]" {
+		if len(string(out)) == 2 {
 			return "", false
+		}
+		result := out
+		if len(string(out)) > 2 {
+			result = strings.TrimSpace(string(out))
 		}
 		if result != "" {
 			return result, true
 		}
-
 		message = "Input cannot be empty. Please enter a value."
 	}
 }
